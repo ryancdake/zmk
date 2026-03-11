d61667fefb13a679d89c15e323aa65f20a7d4be8
ryancdake <ryancdake@gmail.com>
2026-03-11 13:56:41 -0500

    Change color format in trllkng image descriptor

--- app/boards/shields/nice_view/widgets/trllkng.c
+++ app/boards/shields/nice_view/widgets/trllkng.c
@@ -113,7 +113,7 @@ const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST LV_ATTRIBUTE_IMG_TRLLKNG u
 };
 
 const lv_img_dsc_t trllkng = {
-    .header.cf = LV_IMG_CF_INDEXED_1BIT,
+    .header.cf = LV_COLOR_FORMAT_I1,
     .header.always_zero = 0,
     .header.reserved = 0,
     .header.w = 140,
